a7db008a00090c2605ae04651b00c6537f0d8008
Deepak-Kumar8824 <deepakkumardeepakkumar06308@gaim.com>
2026-03-14 12:12:22 +0530

    getting from local storage userInfomation

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 // src/api/api.js – Axios instance with auth interceptor
 import axios from 'axios';
-// API configuration updated
+// API configuration updatedgit add .
 // All API requests will automatically use this base URL 
 const API = axios.create({
     baseURL: "http://localhost:5050/api",
@@ -8,7 +8,7 @@ const API = axios.create({
   
 // Attach JWT token to every request if available
 API.interceptors.request.use((config) => {
-    // getting from local storage userInfo
+    // getting from local storage userInfomation 
     const userInfo = localStorage.getItem('userInfo');
 
     // Check if userInfo exists
